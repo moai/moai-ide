ee885851a43a969e16b353abe7250216003fc8ba
James Rhodes <jrhodes@redpointsoftware.com.au>
2012-02-25 00:10:03 +1100

    Renamed Moai.IDE back to Moai to remove conflicts with pre-existing class.

--- ide/src/Moai/Properties/Resources.Designer.cs
+++ ide/src/Moai/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Moai.IDE.Properties {
+namespace Moai.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Moai.IDE.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Moai.IDE.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Moai.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- ide/src/Moai/Properties/Settings.Designer.cs
+++ ide/src/Moai/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Moai.IDE.Properties {
+namespace Moai.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
